8b9be92a6af6d4061ac0c7dd1daa995ecb5338cc
Adrian Gaudebert <adrian@gaudebert.fr>
2013-03-01 21:29:46 +0100

    Fixes issue #2 - Added compatibility with require.js, removed compatibility with basic script inclusion.

---
+++ tests/browser/test_with_requirejs.js
@@ -0,0 +1,12 @@
+/**
+ * ActionManager JavaScript Library v0.1
+ *
+ * Copyright 2011, Adrian Gaudebert
+ * Licensed under the MIT license.
+ *
+ */
+
+require(['../../action-manager'],
+function (action_manager) {
+    var am = new action_manager.ActionManager();
+});
